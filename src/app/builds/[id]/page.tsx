0856ff38d5e2ae66b547e5edc93725cbafa2396f
schmetty89 <schmetty89@gmail.com>
2026-06-26 01:54:13 -0500

    Update page.tsx

--- src/app/builds/[id]/page.tsx
+++ src/app/builds/[id]/page.tsx
@@ -1,12 +1,13 @@
 import { Navbar } from '@/components/layout/Navbar'
 import { BuildDetail } from '@/components/showcase/BuildDetail'
 
-export default function BuildDetailPage({ params }: { params: { id: string } }) {
+export default async function BuildDetailPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
   return (
     <>
       <Navbar />
       <div className="max-w-4xl mx-auto px-4 py-8">
-        <BuildDetail buildId={params.id} />
+        <BuildDetail buildId={id} />
       </div>
     </>
   )
